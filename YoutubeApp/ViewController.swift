de42572d336f71ef12f211108889580c8ff9a4df
aizawa001 <aizawakyou778@gmail.com>
2020-11-03 17:00:49 +0900

    Initial Commit

---
+++ YoutubeApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  YoutubeApp
+//
+//  Created by 五十嵐慎吉 on 2020/11/03.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
